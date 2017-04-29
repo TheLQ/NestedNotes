5d394bc0b2028f05ed1c46bd0ae598776b37099e
Leon Blakey <leon.m.blakey@gmail.com>
2017-04-29 00:24:46 -0400

    add tslint

--- src/config.ts
+++ src/config.ts
@@ -1,62 +1,62 @@
 export class Root {
-    globalsettings: GlobalSettings
-    notes: Array<Item>
+    public globalsettings: GlobalSettings;
+    public notes: Item[];
 }
 
 export class GlobalSettings {
-    name: String;
+    public name: string;
 }
 
 export class Item {
-    id: number;
-    text: string;
-    settings?: ItemSettings;
-    nested?: Array<Item>;
+    public id: number;
+    public text: string;
+    public settings?: ItemSettings;
+    public nested?: Item[];
 }
 
-export class ItemSettings {   
+export class ItemSettings {
 }
 
 export const example: Root = {
-    "globalsettings": {
-        "name": "myFirstTime"
+    globalsettings: {
+        name: "myFirstTime",
     },
-    "notes": [
+    notes: [
         {
-            "id": 1,
-            "settings": {},
-            "text": "level 1",
-            "nested": [
+            id: 1,
+            settings: {},
+            text: "level 1",
+            nested: [
                 {
-                    "id": 2,
-                    "text": "level 1.1"
+                    id: 2,
+                    text: "level 1.1",
                 },
                 {
-                    "id": 3,
-                    "text": "level 1.2"
-                }
-            ]
+                    id: 3,
+                    text: "level 1.2",
+                },
+            ],
         },
         {
-            "id": 4,
-            "text": "level 2",
-            "nested": [
+            id: 4,
+            text: "level 2",
+            nested: [
                 {
-                    "id": 5,
-                    "text": "level 2.1",
-                    "nested": [
+                    id: 5,
+                    text: "level 2.1",
+                    nested: [
                         {
-                            "id": 6,
-                            "text": "level 2.1.1"
-                        }
-                    ]
+                            id: 6,
+                            text: "level 2.1.1",
+                        },
+                    ],
                 },
                 {
-                    "id": 7,
-                    "text": "level 2.2"
-                }
+                    id: 7,
+                    text: "level 2.2",
+                },
 
-            ]
-        }
-    ]
-}
+            ],
+        },
+    ],
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,38 @@
-"use strict"
+"use strict";
 
-// import jquery from 'jquery';
-// import * as h from './utils';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import * as Config from './config'
+import * as Config from "./config";
 
 class ListProperty {
-    list: Array<Config.Item>
+    list: Config.Item[];
 }
 
 class ListState {
-    list: Array<Config.Item>
+    list: Config.Item[];
 }
 
 class List extends React.Component<ListProperty, ListState> {
-    constructor(props: ListState/*lol*/) {
+    constructor(props: ListProperty) {
         super(props);
         this.state = {
-            list: props.list
-        }
+            list: props.list,
+        };
     }
 
     render(): JSX.Element {
-        let list = this.state.list;
+        const list = this.state.list;
         console.log("list", list);
 
         const nestedItems = list.map((nestedItem) =>
-            <ItemReact key={nestedItem.id} itemTree={nestedItem} />
+            <ItemReact key={nestedItem.id} itemTree={nestedItem} />,
         );
         return (
             <ul>
                 {nestedItems}
             </ul>
-        )
+        );
     }
 }
 
@@ -49,12 +47,13 @@ class ItemState {
 
 class ItemReact extends React.Component<ItemProperty, ItemState> {
     textArea: JSX.Element;
+    itemTree: Config.Item;
 
     constructor(props: ItemProperty) {
         super(props);
         this.state = {
             itemTree: props.itemTree,
-            isTextBox: false
+            isTextBox: false,
         };
 
         // This binding is necessary to make `this` work in the callback
@@ -64,12 +63,12 @@ class ItemReact extends React.Component<ItemProperty, ItemState> {
 
     render() {
         console.log("item", this.state);
-        let item = this.state.itemTree;
-        let nestedList = item.nested !== undefined
+        const item = this.state.itemTree;
+        const nestedList = item.nested !== undefined
             ? <List list={item.nested} />
             : null;
         if (this.state.isTextBox) {
-            this.textArea = <textarea cols={50} rows={1} defaultValue={item.text} onKeyPress={this.onKey}></textarea>
+            this.textArea = <textarea cols={50} rows={1} defaultValue={item.text} onKeyPress={this.onKey} />;
 
             return (
                 <li onClick={this.onEditItem}>
@@ -94,18 +93,18 @@ class ItemReact extends React.Component<ItemProperty, ItemState> {
         e.stopPropagation();
 
         if (this.state.isTextBox) {
-            //sucesfully stopped propagation
+            // sucesfully stopped propagation
             return;
         }
 
         this.setState((prevState, props) => ({
-            isTextBox: !prevState.isTextBox
-        }))
+            isTextBox: !prevState.isTextBox,
+        }));
     }
 
     onKey(e: React.KeyboardEvent<HTMLTextAreaElement>) {
-        console.log(e.charCode)
+        console.log(e.charCode);
     }
 }
 
-ReactDOM.render(<List list={Config.example.notes} />, document.getElementById('replaceMe'));
+ReactDOM.render(<List list={Config.example.notes} />, document.getElementById("replaceMe"));
