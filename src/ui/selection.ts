065a0a98496d613ac88aee2f17678a36cbc0bf12
Leon Blakey <leon.m.blakey@gmail.com>
2017-07-27 22:39:12 -0400

    selection: implemented select previous, cleanup

--- src/ui/selection.ts
+++ src/ui/selection.ts
@@ -50,7 +50,7 @@ function selectionNext() {
 				break;
 			} else  {
 				if (parent.parent == null) {
-					// do nothing, end of docuemnt?
+					// at last entry
 					return;
 				}
 				result = parent.parent;
@@ -59,106 +59,31 @@ function selectionNext() {
 		}
 	}
 
-	// let trigger = false;
-	// iterate(function selectionPump(currentItem: Config.Item): boolean {
-	// 	console.info("tested ", result, currentItem);
-	// 	if (result === currentItem) {
-	// 		trigger = true;
-	// 		return false;
-	// 	} else if (!trigger) {
-	// 		return false;
-	// 	}
-
-	// 	result = currentItem;
-	// 	return true;
-	// });
-
-	// if (result === activeSelection) {
-	// 	throw new Error("did not advance item");
-	// }
-
 	updateSelection(result);
 }
 
 function selectionPrev() {
 	let result: Config.Item = activeSelection;
-	let trigger = false;
-	iterate(function selectionPump(currentItem: Config.Item): boolean {
-		if (result === currentItem) {
-			return true;
-		} else {
-			result = currentItem;
-			return false;
-		}
-	});
 
-	if (result === activeSelection) {
-		throw new Error("did not advance item");
+	while (true) {
+		const parent = Config.getParent(result);
+		if (parent.indexOfChild != 0) {
+			result = Config.getItem(parent.parentChildren[parent.indexOfChild - 1])
+			while (result.children.length != 0) {
+				result = Config.getItem(result.children[result.children.length - 1]);
+			}
+			break;
+		} else  {
+			if (parent.parent == null) {
+				// at first entry
+				return;
+			}
+			result = parent.parent;
+			break;
+		}
 	}
 
 	updateSelection(result);
-}
-
-function iterate(test: IteratorUntilTrue) {
-	/* Start: could be extracted if needed */
-	Config.getActiveConfig().notes.forEach(function iteratePump(rootItem, i) {
-		console.log("iterating children of " + rootItem.text + " index " + i);
-		let current = rootItem;
-		const stack: Config.Item[] = [];
-		while (true) {
-			console.log("stack " + stack.length + " current ", current);
-			current = _iterateNext(stack, current, test);
-			if (current === BREAK_ITEM) {
-				break;
-			} else if (stack.length === 0) {
-				throw new Error("stack length 0");
-			}
-		}
-	});
-	/* End: could be extracted if needed */
-}
-
-const CODE_BREAK = -1;
-
-const BREAK_ITEM: Config.Item = new Config.Item();
-
-function _iterateNext(stack: Config.Item[], currentItem: Config.Item, test: IteratorUntilTrue): Config.Item/*next*/ {
-	if (test(currentItem)) {
-		console.info("test matched", currentItem);
-		return BREAK_ITEM;
-	}
-
-	// descend directly to nested item
-	if (currentItem.nested && currentItem.nested.length > 0) {
-		stack.push(currentItem);
-		return currentItem.nested[0];
-	}
-
-	// find usable parent
-	let stackChild = currentItem;
-	while (stack.length !== 0) {
-		const stackCurrent = stack[stack.length - 1];
-		if (!stackCurrent.nested) {
-			// make compiler happy
-			throw new Error("parent doesn't contain it's child?");
-		}
-
-		let currentIndex = stackCurrent.nested.indexOf(stackChild);
-		if (currentIndex === -1) {
-			throw new Error("couldn't find child in parent?");
-		}
-		if (currentIndex < stackCurrent.nested.length - 1) {
-			// next
-			return stackCurrent.nested[++currentIndex];
-		} else {
-			// done with parent
-			stack.pop();
-			stackChild = stackCurrent;
-		}
-	}
-
-	console.error("no usable parent found");
-	return BREAK_ITEM;
 }
 
 document.addEventListener("keypress", function selectionKeyPressListener(e: KeyboardEvent) {
@@ -167,10 +92,8 @@ document.addEventListener("keypress", function selectionKeyPressListener(e: Keyb
 		return;
 	}
 	if (e.charCode === "s".charCodeAt(0)) {
-		e.stopPropagation();
 		selectionNext();
 	} else if (e.charCode === "w".charCodeAt(0)) {
-		e.stopPropagation();
 		selectionPrev();
 	}
 });
