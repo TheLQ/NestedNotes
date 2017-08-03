cd54a63703f421abf55520d47e4484f0e9df9829
Leon Blakey <leon.m.blakey@gmail.com>
2017-08-02 21:04:01 -0400

    throw alert on error so know when to stop using

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,33 @@ import * as View from "./ui/view";
 // initialize
 import "./ui/edit";
 import "./ui/insert";
+import "./ui/move";
 import "./ui/selection";
 
 // window.addEventListener('error', (e) => {
 //     // e instanceof ErrorEvent
 //     console.error('caught the error: ', e);
 // });
+
+function globalErrorHandler(event: ErrorEvent) {
+	// Note that col & error are new to the HTML 5 spec and may not be
+	// supported in every browser.  It worked for me in Chrome.
+	let extra = !event.colno ? "" : "\ncolumn: " + event.colno;
+	extra += !event.error ? "" : "\nerror: " + event.error;
+
+	// You can view the information in an alert to see things working like this:
+	alert("Error: " + event.message + "\nfilename: " + event.filename + "\nline: " + event.lineno + extra);
+
+	// TODO: Report this error via ajax so you can keep track
+	//       of what pages have JS issues
+
+	const suppressErrorAlert = true;
+	// If you return true, then error alerts (like in older versions of
+	// Internet Explorer) will be suppressed.
+	return suppressErrorAlert;
+}
+
+window.addEventListener("error", globalErrorHandler);
 
 window.addEventListener("load", main);
 
@@ -38,7 +59,7 @@ export function main() {
 }
 
 export function makeReactComponent() {
-	return <View.ViewComponent initRenderer={View.defaultRenderer}/>;
+	return <View.ViewComponent initRenderer={View.defaultRenderer} />;
 }
 
 console.log("main end");
