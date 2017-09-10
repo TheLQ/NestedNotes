f8eab8a5c57b81fa69831cd1c5110a31480ab72d
Leon Blakey <leon.m.blakey@gmail.com>
2017-09-10 19:05:55 -0400

    gulpfile cleanup

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,82 +1,22 @@
-/* To run: npm run gulp -- build
- */
-
-import * as es from "event-stream";
-import * as fs from "fs";
-import * as debug from "gulp-debug";
+// import "ts-babel-node/register-babel";
 
 import * as gulp from "gulp";
-import * as babel from "gulp-babel";
-import tslint from "gulp-tslint";
-import * as ts from "gulp-typescript";
-import * as gutil from "gulp-util";
-import * as rename from "gulp-rename";
-
 import * as gulpStd from "gulp-standard-tasks";
-const tasks = gulpStd(gulp);
-
-import * as Karma from "karma";
-
+import tslint from "gulp-tslint";
+import * as rollup from "rollup";
 import * as commonjs from "rollup-plugin-commonjs";
 import * as nodeResolve from "rollup-plugin-node-resolve";
 import * as replace from "rollup-plugin-replace";
 import * as rollupTypescript from "rollup-plugin-typescript2";
-import * as rollupBabel from "rollup-plugin-babel";
-// import * as rollup from "rollup-stream";
-// import * as rollup from "gulp-better-rollup";
+import typescript from "typescript";
 
-import * as rollup from "rollup";
-// const rollup = require('rollup');
+/* To run: npm run gulp -- build
+ */
 
-import * as source from "vinyl-source-stream";
-import * as buffer from "vinyl-buffer";
-
-import * as pipe from 'multipipe';
-import * as path from 'path';
-
-// import sass from "gulp-sass";
-// import autoprefixer from "gulp-autoprefixer";
-import * as sourcemaps from "gulp-sourcemaps";
+const tasks = gulpStd(gulp);
 
 const SOURCE_ALL_FILES = "./src/**/*";
 const TEST_ALL_FILES = "./test/**/*";
-
-function newTsProject() {
-	let proj = ts.createProject("tsconfig.json");
-	return proj;
-}
-
-function convertTsToJs(source: string, dest: string): NodeJS.ReadWriteStream {
-	const tsProject = newTsProject();
-	return gulp.src([source])
-		.pipe(tsProject())
-		.on("end", () => {
-			gutil.log(
-				"Compiling typescript in " + source
-				+ " to js in " + dest,
-			);
-		})
-		.js
-		// .pipe(babel({
-		//   presets: ['es2015']
-		// }))
-		.pipe(sourcemaps.write(".", {}))
-		.pipe(gulp.dest(dest));
-}
-
-gulp.task("clean", tasks.clean("./build/src"));
-
-gulp.task("testClean", tasks.clean("./build/test"));
-
-function taskCompile() {
-	return convertTsToJs(SOURCE_ALL_FILES, "./build/src");
-}
-gulp.task("compile", ["clean"], taskCompile);
-
-function taskTestCompile() {
-	return convertTsToJs(TEST_ALL_FILES, "./build/test");
-}
-gulp.task("testCompile", ["testClean"], taskTestCompile);
 
 function doLint(paths: string[]) {
 	return gulp.src(paths)
@@ -87,14 +27,8 @@ function doLint(paths: string[]) {
 			emitError: false,
 		}));
 }
-
-gulp.task("lint", ["compile"], () => {
-	return doLint([SOURCE_ALL_FILES, "gulpfile.ts"]);
-});
-
-gulp.task("testLint", ["testCompile"], () => {
-	return doLint([TEST_ALL_FILES]);
-});
+gulp.task("lint", ["compile"], () => doLint([SOURCE_ALL_FILES, "gulpfile.ts"]));
+gulp.task("testLint", ["testCompile"], () => doLint([TEST_ALL_FILES]));
 
 gulp.task("cleanBundle", tasks.clean("./dist/bundle.js*"));
 
@@ -114,17 +48,17 @@ gulp.task("bundle", async () => {
 				sourceMap: true,
 				namedExports: {
 					"node_modules/react/react.js": [ "Children", "Component", "createElement" ]
-				}
+				},
 			}),
 			rollupTypescript({
-				typescript: require("typescript"),
+				typescript,
 				abortOnError: false,
 				// verbosity: 3,
 				// check: false,
 			}),
 			// needed for react and react-dom
 			replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
-		]
+		],
 	});
 
 	await bundle.write({
@@ -143,105 +77,3 @@ gulp.task("build", ["compile", "testCompile", "bundle"], () => {
 	//   "bundle"
 	// )
 });
-
-gulp.task("watch", ["bundle"], () => {
-	return gulp.watch(
-		[SOURCE_ALL_FILES, TEST_ALL_FILES],
-		{
-			debounceDelay: 5000/*ms*/,
-		},
-		["bundle"],
-	);
-});
-
-gulp.task("test", [/*"bundle""lint", "testLint", "build"*/], () => {
-	new Karma.Server({
-		plugins: [
-			// require("karma-es6-shim"),
-			//
-			require("karma-rollup-plugin"),
-			require("karma-typescript"),
-			require("karma-mocha"),
-			require("karma-sinon"),
-			require("karma-chai"),
-			require("karma-phantomjs-launcher"),
-			// require("karma-coverage"),
-			require("karma-sourcemap-loader"),
-			require("karma-chrome-launcher"),
-			// require("karma-spec-reporter")
-		],
-
-		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: ".",
-
-		// frameworks to use
-		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ["karma-typescript", "mocha", "chai", "sinon"],
-
-		// list of files / patterns to load in the browser
-		files: [
-			"test/**/*.tsx",
-			"test/**/*.ts",
-			"src/**/*.ts",
-			"src/**/*.tsx"
-		],
-
-		// list of files to exclude
-		exclude: [
-		],
-
-		karmaTypescriptConfig: {
-			compilerOptions: {
-				module: "commonjs",
-				sourceMap: true
-			},
-			tsconfig: "./tsconfig.json",
-		},
-
-		preprocessors: {
-			'src/**/*.ts': ['karma-typescript', 'sourcemap'],
-			'src/**/*.tsx': ['karma-typescript', 'sourcemap'],
-			'test/**/*.ts': ['karma-typescript', 'sourcemap'],
-			'test/**/*.tsx': ['karma-typescript', 'sourcemap'],
-		},
-
-		// test results reporter to use
-		// possible values: "dots", "progress"
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ["progress", "karma-typescript"],
-
-		// web server port
-		port: 9876,
-
-		// enable / disable colors in the output (reporters and logs)
-		colors: true,
-
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: "config.LOG_DEBUG",
-
-		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
-
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ["Chrome"/*, "Firefox","PhantomJS"*/],
-
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: false,
-
-		// Concurrency level
-		// how many browser should be started simultaneous
-		concurrency: Infinity,
-	} as Karma.ConfigOptions).start();
-});
-
-// gulp.task("styles", () => {
-//   return gulp.src(paths.src)
-//     // .pipe(sourcemaps.init())
-//     // .pipe(sass.sync().on("error", plugins.sass.logError))
-//     // .pipe(autoprefixer())
-//     // .pipe(sourcemaps.write("."))
-//     .pipe(gulp.dest(paths.dest));
-// });
