f19d19fdf7bb0461be6d95998cc833b4a871acb8
Leon Blakey <leon.m.blakey@gmail.com>
2017-11-07 20:21:58 -0500

    production cleanup

--- src-php/json.php
+++ src-php/json.php
@@ -4,7 +4,6 @@ require 'utils.php';
 
 if (isset($_GET["mode"]) && $_GET["mode"] === "save") {
     // Reparse json for pretty printing
-    die('nope');
     $json = json_decode($_POST['json']);
     saveData($json);
     // print_r($_POST['json']);
--- src/storage/PhpStorageDriver.ts
+++ src/storage/PhpStorageDriver.ts
@@ -3,8 +3,8 @@ import { WebStorageDriver } from "./WebStorageDriver";
 export class PhpStorageDriver extends WebStorageDriver {
 	public constructor() {
 		super(
-			"../src-php/json.php",
-			"../src-php/json.php?mode=save",
+			"src-php/json.php",
+			"src-php/json.php?mode=save",
 		);
 	}
 }
